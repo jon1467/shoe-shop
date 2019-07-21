ec1e6d947630b650a348385cd484fae57bebe013
Jonathan Pepler <jonathan.pepler@gmail.com>
2019-07-21 10:38:51 +0100

    Use Bootstrap

--- web/app/javascript/packs/application.js
+++ web/app/javascript/packs/application.js
@@ -15,6 +15,8 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
+import '../stylesheets/application'
+
 // Support component names relative to this directory:
 var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
