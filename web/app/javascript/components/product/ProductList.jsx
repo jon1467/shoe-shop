2d8a53d8c868787825fdcb7fb7fbf9be58296c1d
Jonathan Pepler <jonathan.pepler@gmail.com>
2019-07-23 01:16:30 +0100

    Show total in basket list

--- web/app/javascript/components/product/ProductList.jsx
+++ web/app/javascript/components/product/ProductList.jsx
@@ -4,34 +4,39 @@ import PropTypes from 'prop-types'
 import ProductListItem from './ProductListItem'
 
 class ProductList extends React.Component {
-  constructor () {
-    super()
+  constructor (props) {
+    super(props)
 
-    this.goToProductPage = this.goToProductPage.bind(this)
+    this.total = this.total.bind(this)
+
+    this.state = {}
+    this.state.productList = Object.entries(props.productList).map(([index, listItem]) => {
+      return {
+        product: listItem['product'],
+        quantity: listItem['quantity'],
+        imagePath: listItem['imagePath'],
+        productPath: listItem['productPath']
+      }
+    })
   }
 
-  goToProductPage () {
-    window.location.href = this.props.productURL
+  total () {
+    const reducer = (acc, value) => {
+      return acc + value.quantity * parseFloat(value.product.price)
+    }
+    return this.state.productList.reduce(reducer, 0)
   }
 
   render () {
     if (Object.entries(this.props.productList).length === 0) {
       return <React.Fragment>Your basket is empty.</React.Fragment>
     }
-
     return (
       <React.Fragment>
-        {Object.entries(this.props.productList).map(([index, listItem]) => {
-          return (
-            <ProductListItem
-              product={listItem['product']}
-              quantity={listItem['quantity']}
-              imagePath={listItem['imagePath']}
-              productPath={listItem['productPath']}
-              key={index}
-            />
-          )
+        {this.state.productList.map((listItem, index) => {
+          return <ProductListItem {...listItem} key={index}/>
         })}
+        <div className="product-list__total money">£{this.total()}</div>
       </React.Fragment>
     )
   }
